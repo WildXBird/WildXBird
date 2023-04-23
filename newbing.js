81a5add73fa619186947a1a1c721807e3dff3df6
John Doe <xieqiqiang00@live.com>
2023-04-23 14:57:50 +0800

    Update newbing.js

--- newbing.js
+++ newbing.js
@@ -1,25 +1,43 @@
-setInterval(() => {
-    const pops = document.getElementsByTagName("cib-serp")[0].shadowRoot.getRootNode().getElementById("cib-conversation-main").shadowRoot.getRootNode().getElementById("cib-chat-main").children
-    console.log("pops", pops)
-    for (let pop of pops) {
-        if (pop.tagName === "CIB-CHAT-TURN") {
-            const children = pop.shadowRoot.getRootNode().children
-            for (let child of children) {
-                if (child.tagName === "CIB-MESSAGE-GROUP") {
-                    if (child.getAttribute("source") === "user") {
-                        const textBlock = child.shadowRoot.getRootNode().children[0].shadowRoot.getRootNode().children[0].children[0]
-                        textBlock.style.whiteSpace = "break-spaces"
-                    } else {
-                        const blocks = child.shadowRoot.getRootNode().children
-                        for (let block of blocks) {
-                            if (block.getAttribute("type") === "text") {
-                                for (let messageBolck of block.shadowRoot.getRootNode().children) {
-                                    if (messageBolck.tagName === "CIB-SHARED") {
-                                        const textBlock = messageBolck.children[0]
+// ==UserScript==
+// @name         new Bing 样式修复
+// @namespace    https://www.bing.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://www.bing.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
 
-                                        textBlock.style.whiteSpace = "break-spaces"
+(function () {
+    'use strict';
+    setInterval(() => {
+        const pops = document.getElementsByTagName("cib-serp")[0].shadowRoot.getRootNode().getElementById("cib-conversation-main").shadowRoot.getRootNode().getElementById("cib-chat-main").children
+        // console.log("pops", pops)
+        for (let pop of pops) {
+            if (pop.tagName === "CIB-CHAT-TURN") {
+                const children = pop.shadowRoot.getRootNode().children
+                for (let child of children) {
+                    if (child.tagName === "CIB-MESSAGE-GROUP") {
+                        if (child.getAttribute("source") === "user") {
+                            const textBlock = child.shadowRoot.getRootNode().children[0].shadowRoot.getRootNode().children[0].children[0]
+                            textBlock.style.whiteSpace = "break-spaces"
+                        } else {
+                            const blocks = child.shadowRoot.getRootNode().children
+                            for (let block of blocks) {
+                                if (block.getAttribute("type") === "text") {
+                                    for (let messageBolck of block.shadowRoot.getRootNode().children) {
+                                        if (messageBolck.tagName === "CIB-SHARED") {
+                                            console.log("messageBolck", messageBolck)
+                                            const textBlock = messageBolck.children[0]
+                                            textBlock.style.whiteSpace = "break-spaces"
+                                            const sups = textBlock.getElementsByTagName("sup")
+                                            for (let sup of sups) {
+                                                sup.style.userSelect = "none"
+                                            }
+                                        }
+
                                     }
-
                                 }
                             }
                         }
@@ -27,5 +45,5 @@ setInterval(() => {
                 }
             }
         }
-    }
-}, 1000);
+    }, 1000);
+})();
